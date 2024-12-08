b648367e599a5c0bda92c736800f31d845c9c00d
Bgabri <76167293+Bgabri@users.noreply.github.com>
2024-12-08 08:26:39 +0100

    day 08

---
+++ Day08.hx
@@ -0,0 +1,152 @@
+using StringTools;
+using PrimitiveTools;
+using Lambda;
+import Math.*;
+
+// haxe --interp --main Day08.hx
+typedef Input = Array<Array<String>>;
+class Day08 {
+    static function main() {
+        trace("solution to part 1: " + part1(loadFile("inputs/08.txt")));
+        trace("solution to part 2: " + part2(loadFile("inputs/08.txt")));
+    }
+
+    static function part1(input:Input) {
+        var total = 0;
+
+
+        var m:Map<String, Array<{x:Int, y:Int}>> = [];
+
+
+        var aa:Array<Array<String>> = [];
+        for (y in 0...input.length) {
+            var line = input[y];
+            aa.push([]);
+            for (x in 0...line.length) {
+                var v = line[x];
+                var line = input[y];
+                aa[y].push(" ");
+
+                if (v != ".") {
+                    if (m.exists(v)) {
+                        m.get(v).push({x: x, y:y});
+                    } else {
+                        m.set(v, [{x: x, y:y}]);
+                    }
+                    aa[y][x] = " ";
+                }
+            }
+        }
+
+        var ps:Array<{x:Int, y:Int}> = [];
+        for (key => val in m) {
+
+            for (i in 0...val.length) {
+                var p1 = val[i];
+                for (j in i+1...val.length) {
+                    var p2 = val[j];
+
+                    var mx = p2.x - p1.x;
+                    var my = p2.y - p1.y;
+                    
+                    var anti = {x: p1.x - mx, y: p1.y - my};
+                    var anti2 = {x: p2.x + mx , y: p2.y + my};
+
+                    if (0 <= anti.y && anti.y < aa.length && 0 <= anti.x && anti.x < aa[anti.y].length && aa[anti.y][anti.x] == " ") {
+                        aa[anti.y][anti.x] = "#";
+                        total++;
+                    }
+
+                    if (0 <= anti2.y && anti2.y < aa.length && 0 <= anti2.x && anti2.x < aa[anti2.y].length && aa[anti2.y][anti2.x] == " ") {
+                        aa[anti2.y][anti2.x] = "#";
+                        total++;
+                    }
+                }
+            }
+        }
+        return total;
+    }
+
+    static function part2(input:Input) {
+        var total = 0;
+
+
+        var m:Map<String, Array<{x:Int, y:Int}>> = [];
+
+        var aa:Array<Array<String>> = [];
+
+        for (y in 0...input.length) {
+            aa.push([]);
+            var line = input[y];
+            for (x in 0...line.length) {
+                aa[y].push(" ");
+                var v = line[x];
+                if (v != ".") {
+                    if (m.exists(v)) {
+                        m.get(v).push({x: x, y:y});
+                    } else {
+                        m.set(v, [{x: x, y:y}]);
+                    }
+                }
+            }
+        }
+
+        var ps:Array<{x:Int, y:Int}> = [];
+        for (key => val in m) {
+
+            for (i in 0...val.length) {
+                var p1 = val[i];
+                for (j in i+1...val.length) {
+                    var p2 = val[j];
+
+                    var mx = p2.x - p1.x;
+                    var my = p2.y - p1.y;
+                    var i = 0;
+                    while (true) {
+                        var anti = {x: p1.x - i*mx, y: p1.y - i*my};
+                        i++;
+
+                        if (!(0 <= anti.y && anti.y < aa.length && 0 <= anti.x && anti.x < aa[anti.y].length)) {
+                            break;
+                        }
+                        if (aa[anti.y][anti.x] == " ") {
+                             aa[anti.y][anti.x] = "#";
+                             total++;
+                        }
+                    }
+
+                    var i = 0;
+                    while (true) {
+                        var anti = {x: p1.x + i*mx, y: p1.y + i*my};
+                        i++;
+
+                        if (!(0 <= anti.y && anti.y < aa.length && 0 <= anti.x && anti.x < aa[anti.y].length)) {
+                            break;
+                        }
+                         if (aa[anti.y][anti.x] == " ") {
+                             aa[anti.y][anti.x] = "#";
+                             total++;
+                        }
+                    }
+                }
+            }
+        }
+        return total;
+    }
+
+    static function loadFile(file:String):Input {
+        var input:Input = [];
+        var iterator = sys.io.File.read(file, false);
+
+        while (!iterator.eof()) {
+            var line = iterator.readLine();
+            if (line == "")
+                continue;
+
+            var inp = line.split("");
+            input.push(inp);
+        }
+        iterator.close();
+        return input;
+    }
+}
